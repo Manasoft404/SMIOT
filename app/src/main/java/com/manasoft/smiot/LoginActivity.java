4a6ea65556c0af5940a6143abe221f827b22a235
Manasoft404 <ngudiamanasse@gmail.com>
2018-06-23 09:57:13 -0700

    view login activity

--- app/src/main/java/com/manasoft/smiot/LoginActivity.java
+++ app/src/main/java/com/manasoft/smiot/LoginActivity.java
@@ -2,12 +2,19 @@ package com.manasoft.smiot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login); getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+
     }
 }
